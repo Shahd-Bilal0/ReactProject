6eef0eee816e43256e2c8a7e63b706b550251405
Shahd-Bilal0 <hamzashahd001@gmail.com>
2023-12-06 15:12:28 +0200

    props

--- src/components/ProjectCard/TargetCard.js
+++ src/components/ProjectCard/TargetCard.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./TargetCard.css";
-const TargetRow = ({ icon, color, background, progress, category, value }) => {
+const TargetRow = (props) => {
   return (
     <div className="target">
       <div
         className="icons"
-        style={{ color: color, backgroundColor: background }}
+        style={{ color: props.color, backgroundColor: props.background }}
       >
-        <FontAwesomeIcon className="icon" icon={icon} />
+        <FontAwesomeIcon className="icon" icon={props.icon} />
       </div>
       <div className="details">
-        <span className="category">{category}</span>
-        <span className="value">{value}</span>
-        <div className="progress" style={{ background: background }}>
-          <span style={{ width: progress, backgroundColor: color }}></span>
+        <span className="category">{props.category}</span>
+        <span className="value">{props.value}</span>
+        <div className="progress" style={{ background: props.background }}>
+          <span
+            style={{ width: props.progress, backgroundColor: props.color }}
+          ></span>
         </div>
       </div>
     </div>
--- src/components/ProjectCard/TicketCard.js
+++ src/components/ProjectCard/TicketCard.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./TicketCard.css";
-const TicketCard = ({ icon, count, label, color }) => {
+const TicketCard = (props) => {
   return (
     <div className="box">
-      <FontAwesomeIcon className="icon" icon={icon} style={{ color: color }} />
-      <span className="d-block text-black fw-bold fs-5 mb-1 ">{count}</span>
-      {label}
+      <FontAwesomeIcon
+        className="icon"
+        icon={props.icon}
+        style={{ color: props.color }}
+      />
+      <span className="d-block text-black fw-bold fs-5 mb-1 ">
+        {props.count}
+      </span>
+      {props.label}
     </div>
   );
 };
