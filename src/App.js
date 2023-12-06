c38906fa7e9fc5412bdf377e191eb431c8abdec0
Shahd-Bilal0 <hamzashahd001@gmail.com>
2023-12-06 14:53:38 +0200

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -7,60 +7,65 @@ import UseState from "./components/ProjectCard/UseState";
 import "./App.css";
 
 const App = () => {
+  const targetsData = [
+    {
+      icon: "fa-dollar-sign",
+      color: "blue",
+      background: "rgb(0 117 255 / 20%)",
+      progress: "90%",
+      category: "Money",
+      value: "$20.000",
+    },
+    {
+      icon: "fa-code",
+      color: "orange",
+      background: "rgb(245 158 11 / 20%)",
+      progress: "80%",
+      category: "Projects",
+      value: "24",
+    },
+    {
+      icon: "fa-user",
+      color: "green",
+      background: "rgb(34 197 94 / 20%)",
+      progress: "55%",
+      category: "Team",
+      value: "12",
+    },
+  ];
+  const ticketsData = [
+    {
+      icon: "fa-rectangle-list fa-2x",
+      count: 2500,
+      label: "Total",
+      color: "orange",
+    },
+    { icon: "fa-spinner fa-2x", count: 500, label: "Pending", color: "blue" },
+    {
+      icon: "fa-circle-check fa-2x",
+      count: 1900,
+      label: "Closed",
+      color: "green",
+    },
+    {
+      icon: "fa-rectangle-xmark fa-2x",
+      count: 100,
+      label: "Deleted",
+      color: "red",
+    },
+  ];
   return (
     <div className="wrapper">
       <MainCard heading="Main card Targets" description="first component">
-        <TargetCard
-          icon="fa-dollar-sign"
-          color="blue"
-          background="rgb(0 117 255 / 20%)"
-          progress="90%"
-          category="Money"
-          value="$20.000"
-        />
-        <TargetCard
-          icon="fa-code"
-          color="orange"
-          background="rgb(245 158 11 / 20%)"
-          progress="80%"
-          category="Projects"
-          value="24"
-        />
-        <TargetCard
-          icon="fa-user"
-          color="green"
-          background="rgb(34 197 94 / 20%)"
-          progress="55%"
-          category="Team"
-          value="12"
-        />
+        {targetsData.map((target, index) => (
+          <TargetCard key={index} {...target} />
+        ))}
       </MainCard>
       <MainCard heading="Main Card Statistics" description="second component">
         <TicketsWrapper>
-          <TicketCard
-            icon="fa-rectangle-list fa-2x"
-            count={2500}
-            label="Total"
-            color="orange"
-          />
-          <TicketCard
-            icon="fa-spinner fa-2x"
-            count={500}
-            label="Pending"
-            color="blue"
-          />
-          <TicketCard
-            icon="fa-circle-check fa-2x"
-            count={1900}
-            label="Closed"
-            color="green"
-          />
-          <TicketCard
-            icon="fa-rectangle-xmark fa-2x"
-            count={100}
-            label="Deleted"
-            color="red"
-          />
+          {ticketsData.map((ticket, index) => (
+            <TicketCard key={index} {...ticket} />
+          ))}
         </TicketsWrapper>
       </MainCard>
       <MainCard heading="Main Card useState" description="third component">
