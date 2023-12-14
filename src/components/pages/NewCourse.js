ce025b358036a53362f59047ef24f42cecdeb2db
Shahd-Bilal0 <hamzashahd001@gmail.com>
2023-12-14 14:51:06 +0200

    Addnew

--- src/components/pages/NewCourse.js
+++ src/components/pages/NewCourse.js
@@ -1,27 +1,67 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const NewCourse = () => {
+  const [title, setTitle] = useState("add title");
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState("img/course-04.jpg");
+  const [rating, setRating] = useState("1");
+  const [user, setUser] = useState("");
+  const [price, setPrice] = useState("");
+  const [isPending, setIsPending] = useState(false);
+  const hist = useHistory();
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const newCourse = { img, title, description, user, price, rating };
+    setIsPending(true);
+    fetch("http://localhost:8000/coursesData", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCourse),
+    }).then(() => {
+      setIsPending(false);
+      //   hist.go(-1);
+      //   hist.go(1);
+      hist.push("/courses");
+    });
+  };
   return (
     <div>
       <h1 className="pageTitle">Add New Course</h1>
-      <div className="courseInfo">
-        <form className="addCourse">
+      <div className="new">
+        <form onSubmit={handleAdd} className="addCourse">
           <label>
             Course title:
-            <input type="text" required />
+            <input
+              type="text"
+              required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </label>
 
           <label>
             Course description:
-            <textarea name="description" required />
+            <textarea
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
           </label>
 
           <label>
             Course Image URL:
-            <input type="text" />
+            <input
+              type="text"
+              value={img}
+              onChange={(e) => setImg(e.target.value)}
+            />
           </label>
 
           <label>
             Rate:
-            <select>
+            <select value={rating} onChange={(e) => setRating(e.target.value)}>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -32,15 +72,35 @@ const NewCourse = () => {
 
           <label>
             Participants:
-            <input type="number" required />
+            <input
+              type="number"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              required
+            />
           </label>
 
           <label>
             Course price:
-            <input type="number" required />
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
           </label>
-          <button type="submit">Add Course</button>
+          <div className="button-container">
+            {!isPending && <button type="submit">Add Course</button>}
+            {isPending && (
+              <button disabled type="submit">
+                Waiting...
+              </button>
+            )}
+          </div>
         </form>
+        <div className="image">
+          <img src="img/c.png" alt="" />
+        </div>
       </div>
     </div>
   );
